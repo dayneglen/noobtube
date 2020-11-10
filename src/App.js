89dcd5552a4c7545bce4265850d63affbb1d080f
DylanSironi <sironidylan@gmail.com>
2020-11-10 16:32:42 -0600

    dash

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import './App.scss';
+import Video from './Components/Video'
+import Creator from './Components/Creator';
+import Routes from './Routes';
 
 function App() {
   return (
     <div className="App">
-     
+
+      <Video/>
+      <Creator />
+     {Routes}
     </div>
   );
 }
 
-export default App;
+export default App; 
