9e616bd7dc657fb61de062f611d4893af893e615
DylanSironi <sironidylan@gmail.com>
2020-11-10 11:25:32 -0600

    video-structure

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,1 +1,14 @@
-// header 
+import React, { Component } from 'react';
+import '../Styles/header.scss'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='Header-page'>
+
+            </div>
+        )
+    }
+}
+
+export default (Header);
