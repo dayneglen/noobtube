370ac4c71a866104f88af01ff4fbec047b91704f
DylanSironi <sironidylan@gmail.com>
2020-11-11 16:19:45 -0600

    main

--- src/Components/Dash.js
+++ src/Components/Dash.js
@@ -40,13 +40,8 @@ const Dash = props => {
         return (
             <div className='Dash-page'>
                 <div className='dash-videos'>
-<<<<<<< HEAD
-                    <Header/>
-
-=======
                     {/* <Header/> */}
                   {videos}
->>>>>>> main
                 </div>
             </div>
         )
