4c8b13ce2d5af694297f0885d064cea8baa229da
aquilas91 <agaq2020@gmail.com>
2020-11-11 12:34:34 -0600

    admin

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,45 +1,56 @@
-import React, { Component } from 'react';
-import '../Styles/header.scss'
-// import { Link } from "react-router-dom";
-
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Video from "./Video";
+import "../Styles/header.scss";
 
 class Header extends Component {
-    constructor(){
-        super();
-        this.state ={
-            search:''
-        }
-    }
-    updateSearch = (e) => {
-        this.setState({
-            search: e.target.value.substr(0, 10)});
-    }
+  constructor() {
+    super();
+    this.state = {
+      search: "",
+    };
+  }
+  updateSearch = (e) => {
+    this.setState({
+      search: e.target.value.substr(0, 10),
+    });
+  };
 
-    render() {
-            // let filteredVideo =this.props.Video.filter(
-            //     (video) =>{
-            //         return Video.title.toLowerCase().indexOf(
-            //             this.state.search.toLowerCase()) !== -1 ;
-            //     }
-            // );
-        return (
-            <div className='Header-page'>
-               {/* <Link to="/dash"> <img src={logo} alt="Noobtube" /> </Link> */}
-                <input
-                type="text"
-                value={this.state.search}
-                onChange={this.updateSearch}
-                />
-                <ul>
-                    {/* {filteredVideo.map((video)=>{
+  toDash = () => {
+    this.props.history.push("/dash");
+  };
+
+  render() {
+    // let filteredVideo =this.props.Video.filter(
+    //     (Video) =>{
+    //         return Video.title.toLowerCase().indexOf(
+    //             this.state.search.toLowerCase()) !== -1 ;
+    //     }
+    // );
+    return (
+      <div className="Header-page">
+        <button id="logo" onClick={() => this.toDash()}>
+          {" "}
+          <img
+            src={`https://cdn.discordapp.com/attachments/775427056258777138/776118643510476810/noob_tube_2_by_xavgo2_d2ipfmw-pre.png`}
+            alt="Noobtube"
+          />{" "}
+        </button>
+        <input
+          placeholder="Search"
+          type="text"
+          value={this.state.search}
+          onChange={this.updateSearch}
+        />
+        <ul>
+          {/* {filteredVideo.map((video)=>{
                         return <Video video={video}
                                 key={video.id} />
                     })} */}
-                </ul>
-
-            </div>
-        )
-    }
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default (Header);
+export default withRouter(Header);
