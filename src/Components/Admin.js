f804bae92076365614d4e6b57ca32de99a3a259d
aquilas91 <agaq2020@gmail.com>
2020-11-12 10:02:26 -0600

    admin

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -2,14 +2,28 @@ import React, { Component } from "react";
 import "../Styles/admin.scss";
 import axios from "axios";
 import { connect } from "react-redux";
+import { getUser , getVideo} from '../Redux/Reducers/reducer';
 
 class Admin extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       count: 0,
+      videos: []
     };
   }
+  componentDidMount(){
+    // if (!this.props.user.email){
+    //   this.props.history.push('/')
+    // }
+  }
+
+  getVideos = () => {
+    axios
+      .get(`/api/videos/${this.props.user.user_id}`)
+      .then((res) => this.setState({ videos: res.data }))
+      .catch((err) => console.log(err));
+  };
 
   increment = () => {
     this.setState({ count: this.state.count + 1 });
@@ -28,7 +42,15 @@ class Admin extends Component {
   };
 
   render() {
+    console.log(this.props)
+    const mappedVideos =this.state.videos.map((video, i) =>(
+      <div className='video-box'>
+      <img key={i} src={video.video_url} alt='noobtube video' className='video-preview'/>
+      <button onClick={() => this.handleAction(video.video_id)}>Delete</button>
+  </div>
+))
     return (
+      
       <div className="admin-page">
         <section className="admin-table">
           <h1>ADMIN PORTAL</h1>
@@ -36,10 +58,10 @@ class Admin extends Component {
             <thead>
               <tr>
                 <th>Username</th>
-                <th> Video ID </th>
+                <th> Video_id </th>
                 <th>Title</th>
                 <th>Description </th>
-                <th>Video Link</th>
+                <th>Video </th>
                 <th>Views</th>
                 <th>Likes</th>
                 <th>dislikes</th>
@@ -48,21 +70,23 @@ class Admin extends Component {
                 <th>Action</th>
               </tr>
             </thead>
-
+        
             <tbody>
-              <td> Username1</td>
-              <td>Video ID 1 </td>
-              <td> Title 1</td>
-              <td> description 1</td>
-              <td> video_url 1</td>
-              <td> {this.state.count}</td>
-              <td> {this.state.count}</td>
-              <td> {this.state.count}</td>
-              <td> comment</td>
-              <td> {this.state.count}</td>
-              <td>
-                <button onClick={this.handleAction}>Delete</button>
-              </td>
+              <tr>
+                <td> Username1</td>
+                <td>Video ID 1 </td>
+                <td> Title 1</td>
+                <td> description 1</td>
+                <td> {mappedVideos}</td>
+                <td> {this.state.count}</td>
+                <td> {this.state.count}</td>
+                <td> {this.state.count}</td>
+                <td> comment</td>
+                <td> {this.state.count}</td>
+                <td>
+                  <button onClick={this.handleAction}>Delete</button>
+                </td>
+              </tr>
             </tbody>
           </table>
         </section>
@@ -71,4 +95,8 @@ class Admin extends Component {
   }
 }
 
-export default connect((state) => state, {})(Admin);
+const mapStateToProps = (reduxState) => {
+  return reduxState;
+};
+
+export default connect(mapStateToProps, {getUser, getVideo})(Admin);
